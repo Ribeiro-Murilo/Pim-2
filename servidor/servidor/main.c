f0641673350a052f7646c55de6f73a3f5ee6c00a
Murilo Ribeiro <muriloribeiro11@gmail.com>
2023-10-12 12:28:36 -0300

    add arquivo server

---
+++ servidor/servidor/main.c
@@ -0,0 +1,26 @@
+//
+//  main.c
+//  servidor
+//
+//  Created by Ribeiro on 12/10/23.
+//
+
+#include <stdio.h>
+//sera feito
+
+//print das informações que forem solicitadas
+
+//faz um switch case perguntando oq precisa
+
+//info a serem espostas:
+    //quantidade que cada obra teve venda
+    //quantidade de cada tipo de ingreço (meia inteira izenta)
+    //fazer media das respostas que seram de 0 a 10 e mostras a media que esta sendo aquela obra
+    //oq mais quier
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Servidor aqui/n");
+    return 0;
+}
