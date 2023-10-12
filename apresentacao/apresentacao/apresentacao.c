0d22362a71e61aa130bf26f39053eaa6144af4f0
Murilo Ribeiro <muriloribeiro11@gmail.com>
2023-10-12 12:16:47 -0300

    add arquivo de apresentação

---
+++ apresentacao/apresentacao/apresentacao.c
@@ -0,0 +1,24 @@
+//
+//  main.c
+//  apresentacao
+//
+//  Created by Ribeiro on 12/10/23.
+//
+
+#include <stdio.h>
+//aqui sera feito
+
+// autentificação do codigo anteriormente comprado na bilheteria
+
+// apresentação de acordo com o tiket que foi comprado
+
+// perguntas a respeito da obra apresentada
+
+// perguntas pessoais
+
+//upload dos dados recolhidos para o csv
+
+int main(int argc, const char * argv[]) {
+    printf("Toda a apresentação aqui da obra selecionada!\n");
+    return 0;
+}
