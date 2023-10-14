139a44b002df84fe9f9d2c0fea30cfc9fe596c8a
Murilo Ribeiro <muriloribeiro11@gmail.com>
2023-10-14 19:16:12 -0300

    Bilheteria pronta (logica)

--- Bilheteria/Bilheteria/BilheteriaCodigo.c
+++ Bilheteria/Bilheteria/BilheteriaCodigo.c
@@ -70,6 +70,7 @@ void marcarEntrada(){
             default:
                 break;
         }
+    /*
     int outraEntrada;
     printf("Deseja comprar outro tipo de ingresso?\n");
     do{
@@ -80,7 +81,7 @@ void marcarEntrada(){
         }else if(outraEntrada==2)
             printf("corrigir problema de looping\n");
             //perguntasPessois();
-    }while (!(outraEntrada==1||outraEntrada==2));
+    }while (!(outraEntrada==1||outraEntrada==2));*/
 }
 void compraDeIngressos(){
     int entradaInteira =30;
@@ -109,10 +110,10 @@ void compraTipoEntrada(){
     int entradaMeia =15;
     int entradaIsenta =0;
     
-    printf("Os valores da entrada são:\n- %i Reais para entrada inteira\n- %i Reais para meia entrada\n- %i Reais para entrada isenta.\n",entradaInteira, entradaMeia, entradaIsenta);
+    printf("\nOs valores da entrada são:\n- %i Reais para entrada inteira\n- %i Reais para meia entrada\n- %i Reais para entrada isenta.\n",entradaInteira, entradaMeia, entradaIsenta);
     
     do{
-        printf("Para compara a entrada inteira selecione 1\nPara compara a meia entrada selecione 2\nPara compara a entrada isenta selecione 3\nEntrada: ");
+        printf("\nPara compara a entrada inteira selecione 1\nPara compara a meia entrada selecione 2\nPara compara a entrada isenta selecione 3\nEntrada: ");
         scanf("%i",&tipoEntrada);
     }while (tipoEntrada<1 || tipoEntrada>3);
     printf("Qual a quantidade: ");
@@ -123,17 +124,25 @@ void compraTipoEntrada(){
 
 
 //joga o codigo tokem para dentro do csv;
-void lancarCodigoEentrada(){
+void lancarSistema(){
     //printf("Seu codigo de entrada é: %i",codigoGerado);
     FILE *DBToken;
     DBToken = fopen("/Volumes/Faculdade/faculdade/Pim/codigo/Pim/DBToken.csv", "a");
     if (DBToken == NULL) {
         printf("Erro ao abrir o arquivo.\n");
     }
-    fprintf(DBToken,"\n%s",codigoToken);
+    if(entrada1[1]==1){
+        fprintf(DBToken,"\n%s;%i;%i",codigoToken,entrada1[0],entrada1[2]);
+    }else if (entrada2[1]==2){
+        fprintf(DBToken,"\n%s;%i;%i",codigoToken,entrada2[0],entrada2[2]);
+    }else if (entrada3[1]==3){
+        fprintf(DBToken,"\n%s;%i;%i",codigoToken,entrada3[0],entrada3[2]);
+    }
     fclose(DBToken);
 }
-
+//entrada1[0] = apresentacao;
+//entrada1[1] = tipoEntrada;
+//entrada1[2] = quantEntrada;
 
 bool buscaValidacao() {
     char linha[256];
@@ -186,8 +195,21 @@ void validarToken() {
 }
 
 
-//corrigir quando o cliente quer comprar mais de um tipo de entrada fica em looping
-
+void confirmarCompra(){
+    int quantidadeIngressos;
+    if(entrada1[1]==1){
+        idObraApresentada=entrada1[0];
+        quantidadeIngressos=entrada1[0];
+    }else if (entrada2[1]==2){
+        idObraApresentada=entrada2[0];
+        quantidadeIngressos=entrada2[0];
+    }else if (entrada3[1]==3){
+        idObraApresentada=entrada3[0];
+        quantidadeIngressos=entrada3[0];
+    }
+    selecionarObra();
+    printf("Confirma que deseja comprar %i ingressos para %s?\n",quantidadeIngressos,obraApresentada);
+}
 
 int main() {
     printf("Bilheteria\n");
@@ -209,15 +231,23 @@ int main() {
     compraTipoEntrada();
     
         //gera o codigo
-        gerarCodigo();
+    gerarCodigo();
         
         //busca no csv a validação
-        buscaValidacao();
-        
-        //return 0;
-        validarToken();
-        lancarCodigoEentrada();
-        printf("Codigo valido em %i acessos.\n",entrada1[2]);
-        
+    buscaValidacao();
+    
+    validarToken();
+    
+    //lança no csv 1-token 2-qual obra 3-quantidade comprada
+    lancarSistema();
+    
+        if(entrada1[1]==1){
+            printf("Codigo valido em %i acessos.\n",entrada1[2]);
+        }else if (entrada2[1]==2){
+            printf("Codigo valido em %i acessos.\n",entrada2[2]);
+        }else if (entrada3[1]==3){
+            printf("Codigo valido em %i acessos.\n",entrada3[2]);
+        }
+    confirmarCompra();
     return 0;
 }
