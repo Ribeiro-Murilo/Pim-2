f3a8beebf403cd6ebab95925ac9c8a457af671e7
Murilo Ribeiro <muriloribeiro11@gmail.com>
2023-10-15 15:43:22 -0300

    logica de redução de entradas

--- apresentacao/apresentacao/apresentacao.c
+++ apresentacao/apresentacao/apresentacao.c
@@ -26,11 +26,13 @@
 //variaveia globais
 char codigoToken[5];
 #define TAMANHO_MAX_LINHA 100
-int entradaObra=-1;
-int quantidade=-1;
+int entradaObra;
+int quantidade;
 int entradaAtual;
 int quantidadeAtual;
 int tokenProcurado;
+int quantidadeRestante;
+int valido = -1;  // Inicializa com um valor que indica que o token não foi encontrado
 //
 
 void acessarInfoSistema(){
@@ -42,7 +44,6 @@ void acessarInfoSistema(){
     }
 
     char linha[TAMANHO_MAX_LINHA];
-    int valido = -1;  // Inicializa com um valor que indica que o token não foi encontrado
 
     // Lê cada linha do arquivo
     while (fgets(linha, TAMANHO_MAX_LINHA, DBToken) != NULL) {
@@ -56,30 +57,65 @@ void acessarInfoSistema(){
         if (token == tokenProcurado) {
             entradaObra = entradaAtual;
             quantidade = quantidadeAtual;
-            printf("%i\n%i\n%i",tokenProcurado,entradaObra,quantidade);
+            printf("token:%i\nEntrada: %i\nQuantidade: %i\n",tokenProcurado,entradaObra,quantidade);
             valido=0;
             break;  // Encerra a busca, já encontramos o token colocado pelo user
         }
     }
     if(valido==-1){
-        printf("Codigo invalido");
+        printf("Codigo invalido\n");
     }
         
     fclose(DBToken);
 }
 
+void baixaSistema(){
+    FILE *DBToken = fopen("/Volumes/Faculdade/faculdade/Pim/codigo/Pim/DBToken.csv", "r+");
+    
+    if (DBToken == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+    }
+    char linha[TAMANHO_MAX_LINHA];
+    int entradaNova;
+    int entradaAtual;
+    int quantidadeAtual;
+    int token;
+    // Lê cada linha do arquivo
+    while (fgets(linha, TAMANHO_MAX_LINHA, DBToken) != NULL) {
+        // Extrai os valores de token, entrada e quantidade da linha
+        sscanf(linha, "%d;%d;%d", &token, &entradaAtual, &quantidadeAtual);
+        // Verifica se é o token que estamos procurando
+        if (token == tokenProcurado) {
+            // Atualiza os valores
+            quantidadeRestante = quantidadeAtual-1;
+            // Reescreve a linha no arquivo com os novos valores
+            fseek(DBToken, -strlen(linha), SEEK_CUR);
+            fprintf(DBToken, "%d;%d;%d\n", token, entradaAtual, quantidadeRestante);
+            break;
+        }
+    }
+    if(quantidadeRestante>=1){
+        printf("Esse Token de acesso esta disponivel para uso para mais %i acessos.",quantidadeRestante);
+    }else{
+        printf("Esse Token de acesso não esta mais disponivel para uso ou é invalido.");
+    }
+    fclose(DBToken);
+}
 
 
 int main(int argc, const char * argv[]) {
-    printf("Token: ");
-        fgets(codigoToken, sizeof(codigoToken), stdin);
+    do{
+        printf("Token: ");
+            fgets(codigoToken, sizeof(codigoToken), stdin);
 
-        // Remover o caractere de nova linha (\n) inserido pelo fgets
-    codigoToken[strcspn(codigoToken, "\n")] = '\0';
+            // Remover o caractere de nova linha (\n) inserido pelo fgets
+        codigoToken[strcspn(codigoToken, "\n")] = '\0';
+        
+        tokenProcurado=atoi(codigoToken);
+        acessarInfoSistema();
+    }while(valido==-1);
     
-    tokenProcurado=atoi(codigoToken);
-    acessarInfoSistema();
-    
-    
+    //da baixa do tokem no sistema
+    baixaSistema();
     return 0;
 }
