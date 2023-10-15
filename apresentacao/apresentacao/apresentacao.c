d1630a502d924519e91b43b99de232ff6e17c6c1
Murilo Ribeiro <muriloribeiro11@gmail.com>
2023-10-15 12:13:58 -0300

    add validação do tokem

--- apresentacao/apresentacao/apresentacao.c
+++ apresentacao/apresentacao/apresentacao.c
@@ -6,6 +6,10 @@
 //
 
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
 //aqui sera feito
 
 // autentificação do codigo anteriormente comprado na bilheteria
@@ -18,7 +22,64 @@
 
 //upload dos dados recolhidos para o csv
 
+
+//variaveia globais
+char codigoToken[5];
+#define TAMANHO_MAX_LINHA 100
+int entradaObra=-1;
+int quantidade=-1;
+int entradaAtual;
+int quantidadeAtual;
+int tokenProcurado;
+//
+
+void acessarInfoSistema(){
+    FILE *DBToken = fopen("/Volumes/Faculdade/faculdade/Pim/codigo/Pim/DBToken.csv", "r");
+
+    if (DBToken == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        //return 1;
+    }
+
+    char linha[TAMANHO_MAX_LINHA];
+    int valido = -1;  // Inicializa com um valor que indica que o token não foi encontrado
+
+    // Lê cada linha do arquivo
+    while (fgets(linha, TAMANHO_MAX_LINHA, DBToken) != NULL) {
+        int token;
+        int entradaAtual;
+
+        // Extrai os valores de token e entrada da linha
+        sscanf(linha, "%d;%d;%d", &token, &entradaAtual, &quantidadeAtual);
+
+                // Verifica se é o token que estamos procurando
+        if (token == tokenProcurado) {
+            entradaObra = entradaAtual;
+            quantidade = quantidadeAtual;
+            printf("%i\n%i\n%i",tokenProcurado,entradaObra,quantidade);
+            valido=0;
+            break;  // Encerra a busca, já encontramos o token colocado pelo user
+        }
+    }
+    if(valido==-1){
+        printf("Codigo invalido");
+    }
+        
+    fclose(DBToken);
+}
+
+
+
 int main(int argc, const char * argv[]) {
-    printf("Toda a apresentação aqui da obra selecionada!\n");
+    printf("Token: ");
+        fgets(codigoToken, sizeof(codigoToken), stdin);
+
+        // Remover o caractere de nova linha (\n) inserido pelo fgets
+    codigoToken[strcspn(codigoToken, "\n")] = '\0';
+    
+    tokenProcurado=atoi(codigoToken);
+    acessarInfoSistema();
+    
+    
     return 0;
 }
