60d7932a937c08edeb376da67185bb4ebb893ccd
Murilo Ribeiro <muriloribeiro11@gmail.com>
2023-10-15 21:39:12 -0300

    logica de envio das respostas

--- apresentacao/apresentacao/apresentacao.c
+++ apresentacao/apresentacao/apresentacao.c
@@ -36,6 +36,12 @@ int quantidadeAtual;
 int tokenProcurado;
 int quantidadeRestante;
 int valido = -1;  // Inicializa com um valor que indica que o token não foi encontrado
+char obraApresentada[50];
+int respostaQ1[5];
+int respostaQ2[5];
+int respostaQ3[5];
+int respostaQ4[5];
+int respostaQ5[5];
 
 void acessarInfoSistema(){
     FILE *DBToken = fopen("/Volumes/Faculdade/faculdade/Pim/codigo/Pim/DBToken.csv", "r");
@@ -71,6 +77,10 @@ void acessarInfoSistema(){
     fclose(DBToken);
 }
 
+
+//bug quando a quantidade esta em 10 da baixa no sistema fica 90, acontece com 10,100,1000 etc
+
+
 void baixaSistema(){
     FILE *DBToken = fopen("/Volumes/Faculdade/faculdade/Pim/codigo/Pim/DBToken.csv", "r+");
     
@@ -78,7 +88,6 @@ void baixaSistema(){
         printf("Erro ao abrir o arquivo.\n");
     }
     char linha[TAMANHO_MAX_LINHA];
-    int entradaNova;
     int entradaAtual;
     int quantidadeAtual;
     int token;
@@ -107,6 +116,15 @@ void baixaSistema(){
 void apresentarObra(){
     switch (entradaObra) {
         case 1:
+            printf("\n-----------------------------------------------------------------\n");
+          printf("- Apresentando a seção dos 150 anos de Santos Dumont:\n");
+          printf("- Contexto: Em 2023, o Brasil comemorou os 150 anos de nascimento de Alberto Santos Dumont, considerado o ''Pai da Aviacao''. Para celebrar o legado do inventor, foram realizados diversos eventos em todo o pais, incluindo exposicoes, palestras e eventos culturais.\n");
+          printf("- Exposicao: exposicao sobre a vida e obra de Santos Dumont, A exposição ''Santos Dumont: O Homem que Voou'', realizada no Museu do Amanha, no Rio de Janeiro, foi um dos destaques. A exposicao contou com uma série de objetos e documentos que contam a historia do inventor, incluindo o 14-bis, o primeiro aviao a levantar voo por conta propria.\n");
+          printf("- Evento Cultural: O show 'Santos Dumont: O Homem que Voou'', realizado pelo grupo musical ''Trio de Ouro'', foi um dos destaques. O show contou com uma série de canções que homenageiam o inventor, incluindo ''O Aviador'', de Renato Teixeira.\n");
+          printf("- O legado de Santos Dumont: O legado de Santos Dumont é importante nao apenas para o Brasil, mas também para o mundo. O inventor foi um pioneiro na aviação, e suas contribuições ajudaram a tornar o mundo um lugar mais conectado.\n");
+            printf("\n-----------------------------------------------------------------\n");
+          break;
+        case 2:
           printf("\n-----------------------------------------------------------------\n");
           printf("Apresentando a seçao dos 100 anos da semana de Arte Moderna:\n");
           printf("- Pintores: Anita Malfatti, Emiliano Di Cavalcanti, Tarsila do Amaral, Victor Brecheret, Zina Aita, Cândido Portinari, entre outros.\n");
@@ -115,15 +133,6 @@ void apresentarObra(){
           printf("- Obras: Inspiracoes no cubismo, o futurismo, e o surrealismo\n");
           printf("- Local: Teatro Municipal de São Paulo\n");
           printf("- Contexto Historico: O país comemorava 100 anos de Independência, mas ainda enfrentava problemas como a desigualdade social e a pobreza. O evento também foi influenciado pelo contexto internacional, marcado pela Primeira Guerra Mundial e pela ascensão das vanguardas artisticas.\n");
-            printf("\n-----------------------------------------------------------------\n");
-          break;
-        case 2:
-            printf("\n-----------------------------------------------------------------\n");
-          printf("- Apresentando a seção dos 150 anos de Santos Dumont:\n");
-          printf("- Contexto: Em 2023, o Brasil comemorou os 150 anos de nascimento de Alberto Santos Dumont, considerado o ''Pai da Aviacao''. Para celebrar o legado do inventor, foram realizados diversos eventos em todo o pais, incluindo exposicoes, palestras e eventos culturais.\n");
-          printf("- Exposicao: exposicao sobre a vida e obra de Santos Dumont, A exposição ''Santos Dumont: O Homem que Voou'', realizada no Museu do Amanha, no Rio de Janeiro, foi um dos destaques. A exposicao contou com uma série de objetos e documentos que contam a historia do inventor, incluindo o 14-bis, o primeiro aviao a levantar voo por conta propria.\n");
-          printf("- Evento Cultural: O show 'Santos Dumont: O Homem que Voou'', realizado pelo grupo musical ''Trio de Ouro'', foi um dos destaques. O show contou com uma série de canções que homenageiam o inventor, incluindo ''O Aviador'', de Renato Teixeira.\n");
-          printf("- O legado de Santos Dumont: O legado de Santos Dumont é importante nao apenas para o Brasil, mas também para o mundo. O inventor foi um pioneiro na aviação, e suas contribuições ajudaram a tornar o mundo um lugar mais conectado.\n");
             printf("\n-----------------------------------------------------------------\n");
           break;
         case 3:
@@ -157,6 +166,140 @@ void apresentarObra(){
           break;
     }
 }
+void acharTema(){
+    switch (entradaObra){
+        case 1:
+            strcpy(obraApresentada, "150 anos de Santos Dumont");
+            break;
+        case 2:
+            strcpy(obraApresentada, "100 anos da arte moderna");
+            break;
+        case 3:
+            strcpy(obraApresentada, "Jogos olimpicos de paris 2024");
+            break;
+        case 4:
+            strcpy(obraApresentada, "Inteligencia Artificial");
+            break;
+    }
+}
+
+void lancarRespostas(){
+    
+    FILE *DBObra1 = fopen("/Volumes/Faculdade/faculdade/Pim/codigo/Pim/DBObra1.csv", "a");
+    if (DBObra1 == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+    }
+    
+    FILE *DBObra2 = fopen("/Volumes/Faculdade/faculdade/Pim/codigo/Pim/DBObra2.csv", "a");
+    if (DBObra2 == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+    }
+    
+    FILE *DBObra3 = fopen("/Volumes/Faculdade/faculdade/Pim/codigo/Pim/DBObra3.csv", "a");
+    if (DBObra3 == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+    }
+    
+    FILE *DBObra4 = fopen("/Volumes/Faculdade/faculdade/Pim/codigo/Pim/DBObra4.csv", "a");
+    if (DBObra4 == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+    }
+    switch(entradaObra){
+        case 1:
+            fprintf(DBObra1,"\n%i;%i;%i;%i;%i",respostaQ1[0],respostaQ1[1],respostaQ1[2],respostaQ1[3],respostaQ1[4]);
+            break;
+        case 2:
+            fprintf(DBObra2,"\n%i;%i;%i;%i;%i",respostaQ2[0],respostaQ2[1],respostaQ2[2],respostaQ2[3],respostaQ2[4]);
+            break;
+        case 3:
+            fprintf(DBObra3,"\n%i;%i;%i;%i;%i",respostaQ3[0],respostaQ3[1],respostaQ3[2],respostaQ3[3],respostaQ3[4]);
+            break;
+        case 4:
+            fprintf(DBObra4,"\n%i;%i;%i;%i;%i",respostaQ4[0],respostaQ4[1],respostaQ4[2],respostaQ4[3],respostaQ4[4]);
+            break;
+    }
+    fclose(DBObra1);
+    fclose(DBObra2);
+    fclose(DBObra3);
+    fclose(DBObra4);
+}
+
+void selecionarPerguntas(){
+    char perguntasQ1[5][50] = {
+            "Pergunta 1 obra1",
+            "Pergunta 2 obra1",
+            "Pergunta 3 obra1",
+            "Pergunta 4 obra1",
+            "Pergunta 5 obra1"
+        };
+    char perguntasQ2[5][50] = {
+            "Pergunta 1 obra2",
+            "Pergunta 2 obra2",
+            "Pergunta 3 obra2",
+            "Pergunta 4 obra2",
+            "Pergunta 5 obra2"
+        };
+    char perguntasQ3[5][50] = {
+            "Pergunta 1 obra3",
+            "Pergunta 2 obra3",
+            "Pergunta 3 obra3",
+            "Pergunta 4 obra3",
+            "Pergunta 5 obra3"
+        };
+    char perguntasQ4[5][50] = {
+            "Pergunta 1 obra4",
+            "Pergunta 2 obra4",
+            "Pergunta 3 obra4",
+            "Pergunta 4 obra4",
+            "Pergunta 5 obra4",
+        };
+    switch (entradaObra){
+        case 1:
+            acharTema();
+            printf("Perguntas devem ser respondidas de 0 a 10 em numeral a respeito da obra %s\n",obraApresentada);
+            for (int questao=0; questao<5;questao++) {
+                do{
+                    printf("%s\n%i: ",perguntasQ1[questao],questao+1);
+                    scanf("%i",&respostaQ1[questao]);
+                }while(respostaQ1[questao]<0||respostaQ1[questao]>10);
+            }
+            lancarRespostas();
+            break;
+        case 2:
+            acharTema();
+            printf("Perguntas devem ser respondidas de 0 a 10 em numeral a respeito da obra %s\n",obraApresentada);
+            for (int questao=0; questao<5;questao++) {
+                do{
+                    printf("%s\n%i: ",perguntasQ2[questao],questao+1);
+                    scanf("%i",&respostaQ2[questao]);
+                }while(respostaQ2[questao]<0||respostaQ2[questao]>10);
+            }
+            lancarRespostas();
+            break;
+        case 3:
+            acharTema();
+            printf("Perguntas devem ser respondidas de 0 a 10 em numeral a respeito da obra %s\n",obraApresentada);
+            for (int questao=0; questao<5;questao++) {
+                do{
+                    printf("%s\n%i: ",perguntasQ3[questao],questao+1);
+                    scanf("%i",&respostaQ3[questao]);
+                }while(respostaQ3[questao]<0||respostaQ3[questao]>10);
+            }
+            lancarRespostas();
+            break;
+        case 4:
+            acharTema();
+            printf("Perguntas devem ser respondidas de 0 a 10 em numeral a respeito da obra %s\n",obraApresentada);
+            for (int questao=0; questao<5;questao++) {
+                do{
+                    printf("%s\n%i: ",perguntasQ4[questao],questao+1);
+                    scanf("%i",&respostaQ4[questao]);
+                }while(respostaQ4[questao]<0||respostaQ4[questao]>10);
+            }
+            lancarRespostas();
+            break;
+    }
+}
 int main(int argc, const char * argv[]) {
     do{
         printf("Token: ");
@@ -172,8 +315,10 @@ int main(int argc, const char * argv[]) {
     //da baixa do tokem no sistema
     baixaSistema();
     
-    if(quantidadeRestante>=1){
+    if(quantidadeRestante>=0){
         apresentarObra();
+        selecionarPerguntas();
     }
+    printf("Obrigado por vir");
     return 0;
 }
